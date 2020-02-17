d98f2f3c86e7920145d250f6dc2fb339e9618da5
Alexhb61 <abistagn@ucsc.edu>
2020-02-16 17:41:21 -0800

    Mostly Stable test

--- env.py
+++ env.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from collections import deque
 import random
-import ai_safety_gridworlds 
+import numpy as np
+import cv2
+from ai_safety_gridworlds.ai_safety_gridworlds.environments.island_navigation import IslandNavigationEnvironment
+from ai_safety_gridworlds.ai_safety_gridworlds.environments.shared.safety_game import Actions
 import torch
 
 
 class Env():
   def __init__(self, args): #modified for safety environment
     self.device = args.device
-    self.grid = ai_safety_gridworlds.IslandNavigationEnvironment()
-    actions = list(ai_safety_gridworlds.environments.shared.Actions())[0:5] # i need to remove one action namely quit
+    self.grid = IslandNavigationEnvironment()
+    actions = list(Actions)[0:5] # i need to remove one action namely quit
     self.actions = dict([i, e] for i, e in zip(range(len(actions)), actions))
     self.window = args.history_length  # Number of frames to concatenate
     self.state_buffer = deque([], maxlen=args.history_length)
     self.training = True  # Consistent with model training mode
 
   def _get_state(self): #modified for safety environment
-    state = cv2.resize(self.grid.current_game._board, (84, 84), interpolation=cv2.INTER_LINEAR)
-    return torch.tensor(state, dtype=torch.float32, device=self.device).div_(255)
+    np_ascii_state = self.grid.current_game._board.board
+    raw_cv_state = cv2.UMat(np.unpackbits(np_ascii_state, axis = 1).astype('uint8'))
+    state = cv2.resize(raw_cv_state, (84, 84), interpolation=cv2.INTER_LINEAR)
+    return torch.tensor(state.get(), dtype=torch.float32, device=self.device)
 
   def _reset_buffer(self):
     for _ in range(self.window):
@@ -33,13 +38,18 @@ class Env():
     self.state_buffer.append(observation)
     return torch.stack(list(self.state_buffer), 0)
 
+  def _get_total_reward(self):
+    return self.grid.current_game.the_plot._engine_directives.summed_reward
   def step(self, action): #modified for safety environment
     # Repeat action 4 times, max pool over last 2 frames
     reward, done = 0, False
-    if self.grid.current_game.the_plot.summed_reward is not None:
-       reward -= self.grid.current_game.the_plot.summed_reward
+    if self._get_total_reward() is not None:
+      reward -= self._get_total_reward()
+    else:
+      reward = 0
     self.grid.step(self.actions.get(action))
-    reward += self.grid.current_game.the_plot.summed_reward    
+    if self._get_total_reward() is not None:
+      reward += self._get_total_reward()
     observation = self._get_state()
     self.state_buffer.append(observation)
     # Return state, reward, done
